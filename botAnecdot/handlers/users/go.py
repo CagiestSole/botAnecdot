5499a3539690c23eb1f5c3eb820938ce0173c106
CagiestSole <88030042+CagiestSole@users.noreply.github.com>
2023-01-18 15:44:22 +0300

    Update go.py

--- botAnecdot/handlers/users/go.py
+++ botAnecdot/handlers/users/go.py
@@ -1,3 +1,5 @@
+import re
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
@@ -16,21 +18,23 @@ async def go(message: types.Message):
   await message.answer('Поздравляю, ты начал процедуру генерации анекдотов\nТеперь на каждое твое сообщение бот постарается ответить тебе так, чтобы получился анекдот\n')
   await reading.test1.set()
 
-@dp.message_handler(state=register.test1)
+@dp.message_handler(state=reading.test1)
 
 async def state1(message: types.Message, state: FSMContext):
   answer = message.text
 
   await state.update_data(test1=answer)
-  await reading.test2.set()
-
-@dp.message_handler(state=reading.test2)
-
-async def state2(message: types.Message, state: FSMContext):
-  answer = message.text
-  await state.update_data(test2=answer)
-  data = await state.get_data()
-  first = data.get('test1')
-  second = data.get('test2')
-
-  await message.answer(f'Первое - {first}, второе - {second}\n')
+  await message.answer('Генерация займет некоторое время')
+  text1 = await state.get_data('test1')
+  key_bytes = bytes("123", encoding="utf-8")
+  value_bytes = bytes(text1.get('text1'), encoding="utf-8") 
+  producer_tosend.send('quickstart-events2', key=key_bytes, value=value_bytes) 
+  producer_tosend.flush()
+  for msg in consumer_toget:
+      if (msg.key.decode("utf-8") == '333'):
+         print (msg.value.decode("utf-8"))
+         s = msg.value.decode("utf-8")
+         generated_text = s.replace('[SJ]', '').split("[")[0]
+         break
+  await message.answer(generated_text)
+  await state.finish()
